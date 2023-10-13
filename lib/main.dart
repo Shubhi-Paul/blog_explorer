c30e4633b8a1a6467a7069dd2866ef504cd8a163
Shubhi <shubhi.paul2021@vitstudent.ac.in>
2023-10-13 10:36:48 +0530

    feat : changing from hive to graphql

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,37 @@ import 'data/articles_data.dart';
 import 'package:provider/provider.dart';
 import 'providers/category_provider.dart';
 import 'package:flutter/services.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'contants/config.dart';
 
 void main() async {
   try {
     // check if hive is initialized
     await Hive.initFlutter();
+    print("Hive initialized");
     Hive.registerAdapter(ArticleAdapter());
-    await Hive.openBox<Article>('articlesBox');
-    print("Hive initialized");
-    await Hive.openBox<Article>('bookmarkedArticlesBox');
+    
+    print("Hive articlesBox opened");
+    
     print("Hive bookmarkedArticlesBox opened");
-    print("Hive initialized");
+
+    final HttpLink httpLink = HttpLink(
+      'https://intent-kit-16.hasura.app/api/rest/blogs/',
+    );
+    final AuthLink authLink = AuthLink(
+      getToken: () => 'x-hasura-admin-secret $keySecret', //TODO: correct this
+    );
+
+    final Link link = authLink.concat(httpLink);
+
+    ValueNotifier<GraphQLClient> client = ValueNotifier(GraphQLClient(
+      link: link,
+      cache: GraphQLCache(), //TODO: check the hive usage here
+    ),
+    );
+    var articleBox = await Hive.openBox<Article>('articlesBox');
+    var bookmarkedArticlesBox = Hive.openBox<Article>('bookmarkedArticlesBox');
+
   } catch (e) {
     // print error incase hive is not initalized
     print('Error initializing Hive: $e');
@@ -36,10 +56,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-     SystemChrome.setPreferredOrientations([
-        DeviceOrientation.portraitUp,
-        DeviceOrientation.portraitDown,
-      ]);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Blogs Burg',
       theme: ThemeData(
--- lib/module/api.dart
+++ lib/module/api.dart
@@ -2,56 +2,54 @@ import 'package:blog_explorer/contants/config.dart';
 import 'package:blog_explorer/data/articles_data.dart';
 import 'package:blog_explorer/data/category_data.dart';
 import 'package:http/http.dart' as http;
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'dart:convert';
 import 'dart:math';
 import 'package:hive/hive.dart';
 
 // function for storing the fetched data locally to hive
 Future<void> saveDataToHive(List<Article> articles) async {
+  print("trying to save to hive 2");
   var box = await Hive.openBox<Article>('articlesBox');
   await box.clear();
   await box.addAll(articles);
 }
 
 Future<List<Article>> fetchBlogs() async {
-  const String url = 'https://intent-kit-16.hasura.app/api/rest/blogs/';
-  const String adminSecret = keySecret;
+  
+  const String getBlogsQuery = r'''
+  query GetBlogs {
+    blogs {
+      id
+      image_url
+      title
+      category
+      }
+    }
+  ''';
 
   try {
-    final response = await http.get(Uri.parse(url), headers: {
-      'x-hasura-admin-secret': adminSecret,
-    });
+    final QueryResult result = await client.query(QueryOptions(
+      document: gql(getBlogsQuery),
+    ));
 
-    if (response.statusCode == 200) {
-      // Request successful
-      final List<dynamic> jsonData = json.decode(response.body)['blogs'];
-      List<Article> blogs = jsonData
-          .map((blog) => Article(
-              id: blog['id'],
-              imageUrl: blog['image_url'],
-              title: blog['title'],
+    if (result.hasException) {
+      throw Exception(result.exception.toString());
+    }
 
-              // assigning random categories to the blog as none were mentioned
-              category: getRandomCategory()))
-          .toList();
+    {
+      final List<dynamic> jsonData = result.data?['blogs'];
+      print(jsonData);
+      List<Article> blogs =
+          jsonData.map((blog) => Article(id: blog['id'], imageUrl: blog['image_url'], title: blog['title'], category: getRandomCategory())).toList();
 
-      // saving the data to hive
-      saveDataToHive(blogs);
-      print("Data saved to hive");
+      // print("trying to save to hive 1");
+      saveDataToHive(blogs); // Save data to Hive
+      // print("Data saved to hive");
       return blogs;
-    } else {
-      // Request failed
-      print('Request failed with status code: ${response.statusCode}');
-      print('Response data: ${response.body}');
-
-      // error -> unable to connect due to internal server issue
-      Article error = Article(id: '${response.statusCode}', imageUrl: '', title: 'Internal Server Error. Reload after sometime', category: 'error');
-      print("Internal server error");
-      return [error];
     }
   } catch (e) {
-    print('Error $e');
-
+    print('Error is : $e');
     // error -> unable to connect to server due to no internet connectivity
     Article error = Article(id: '404', imageUrl: '', title: 'Unable to connect to the server. Make sure you are connected to the internet', category: 'error');
     return [error];
